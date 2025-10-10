392904daac10c698cbd72ea3ed4fb92a3bb2c040
Matthew McDermott <mattmcdermott8@gmail.com>
2025-10-10 14:14:09 -0400

    Re-organized some things and starting to add casting

--- src/dftly/nodes/arithmetic.py
+++ src/dftly/nodes/arithmetic.py
@@ -4,7 +4,6 @@ As non-terminals, all args and kwargs to these nodes must be other nodes.
 """
 
 from .base import ArgsOnlyFn, BinaryOp
-from .base import Literal  # noqa: F401
 import polars as pl
 
 
@@ -12,6 +11,7 @@ class Add(ArgsOnlyFn):
     """This non-terminal node represents the addition of multiple expressions.
 
     Example:
+        >>> from dftly.nodes import Literal
         >>> pl.select(Add(Literal(1), Literal(2), Literal(3)).polars_expr).item()
         6
     """
@@ -25,6 +25,7 @@ class Subtract(BinaryOp):
     """This non-terminal node represents the difference between the two inputs x, y -> x - y.
 
     Example:
+        >>> from dftly.nodes import Literal
         >>> pl.select(Subtract(Literal(5), Literal(3)).polars_expr).item()
         2
     """
@@ -38,6 +39,7 @@ class Multiply(ArgsOnlyFn):
     """This non-terminal node represents the multiplication of multiple expressions.
 
     Example:
+        >>> from dftly.nodes import Literal
         >>> pl.select(Multiply(Literal(2), Literal(3), Literal(4)).polars_expr).item()
         24
     """
@@ -57,6 +59,7 @@ class Divide(BinaryOp):
     """This non-terminal node represents the division of two expressions.
 
     Example:
+        >>> from dftly.nodes import Literal
         >>> pl.select(Divide(Literal(6), Literal(3)).polars_expr).item()
         2.0
     """
@@ -70,6 +73,7 @@ class Mean(ArgsOnlyFn):
     """This non-terminal node represents the mean of multiple expressions.
 
     Example:
+        >>> from dftly.nodes import Literal
         >>> pl.select(Mean(Literal(1), Literal(2), Literal(3)).polars_expr).item()
         2.0
     """
@@ -82,6 +86,7 @@ class Min(ArgsOnlyFn):
     """This non-terminal node represents the min of multiple expressions.
 
     Example:
+        >>> from dftly.nodes import Literal
         >>> pl.select(Min(Literal(1), Literal(2), Literal(3)).polars_expr).item()
         1
     """
@@ -94,6 +99,7 @@ class Max(ArgsOnlyFn):
     """This non-terminal node represents the max of multiple expressions.
 
     Example:
+        >>> from dftly.nodes import Literal
         >>> pl.select(Max(Literal(1), Literal(2), Literal(3)).polars_expr).item()
         3
     """
